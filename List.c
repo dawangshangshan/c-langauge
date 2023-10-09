5c63a7a1318fc5daf1175eac01e9298676cc7907
dawangshangshan <2837351402@qq.com>
2023-10-09 23:54:43 +0800

    List first commit

--- List.c
+++ List.c
@@ -75,35 +75,57 @@ void delete_List(List* L, int x)
 	}
 }
 
-void delete_surplus(List* L)
+void delete_surplus(List* *L)
 {
-	List* p = L->next;
+	List* p = (*L)->next;
 	while (p)
 	{
 		List *temp = p;
 		List* del=p->next;
 		while (del)
 		{
-			if (del->date = p->date)
+			if (del->date == p->date)
 			{
 				temp->next = del->next;
 				free(del);
-				break;
+				del = temp->next;
+				continue;
 			}
+			temp = temp->next;
+			if(temp)
+				del = del->next;
 		}
 		p = p->next;
 	}
 	
 }
 //
-void reverse_lsit(List **L)
+void reverse_lsit(List** L)
 {
 	List* p = (*L)->next;
 	List* cur = *L;
 	while (p->next)
 	{
-
+		p = p->next;
 	}
+	List* tail = p;
+	while (1)
+	{
+		cur = *L;
+		if (cur->next == p)
+		{
+			p->next = NULL;
+			(*L)->next = tail;
+			break;
+		}
+		while (cur->next != p)
+		{
+			cur = cur->next;
+		}
+		p->next = cur;
+		p = cur;
+	}
+
 }
 
 
@@ -124,13 +146,80 @@ void  test()
 	Init_List(&M);
 	Insert_L(M, 10);
 	Insert_L(M, 20);
+	Insert_L(M, 20);
+	Insert_L(M, 20);
+	Insert_L(M, 20);
 	Insert_L(M, 30);
+	delete_surplus(&M);
 	print_List(M);
-	Insert_Lis_x(M, 20, 21);
+	reverse_lsit(&M);
 	print_List(M);
 }
 
 int main()
 {
 	test();
+}
+
+
+struct Nodelist {
+	int date;
+	struct Nodelist* next;
+	struct Nodelist* tail;
+};
+typedef struct Nodelist List;
+
+
+void Init_List(List** L)
+{
+	*L = (List*)malloc(sizeof(List));
+	if (*L == NULL)
+	{
+		exit(-1);
+	}
+	(*L)->next = *L;
+	(*L)->tail = *L;
+	(*L)->date = 0;
+}
+
+void Insert_List(List* L, int x)
+{
+	List* temp = L;
+	temp = temp->tail;
+	List* p = (List*)malloc(sizeof(List));
+	p->date = x;
+	p->tail = temp;
+	temp->tail->tail = p;
+	p->next = L;
+	temp->next = p;
+}
+int compare(List *L, List *M)
+{
+	List *l = L->next;
+	List *m = M->next;
+	while (l != L && m != M)
+	{
+		if (l->date != m->date)
+			return 0;
+		else if (l->next == L && m->next == M)
+			return 1;
+		l = l->next;
+		m = m->next;
+	}
+	return 0;
+}
+int main()
+{
+	List *M;
+	List *L;
+	Init_List(&M);
+	Init_List(&L);
+	Insert_List(M, 1);
+	Insert_List(M, 2);
+	Insert_List(M, 3);
+	Insert_List(M, 1);
+	Insert_List(L, 1);
+	Insert_List(L, 1);
+	Insert_List(L, 1);
+	printf("%d",compare(L, M));
 }
